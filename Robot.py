63c8309e1a92de8bbe6d0ceeab1501baa4fba787
Alexis Perles <alexis.perles@etu.umontpellier.fr>
2026-09-23 18:49:08 +0200

    Ajout fichier Robot.py : - Fonction deplacer - Fonction recoit_degats - Correction syntaxe attributs

--- Robot.py
+++ Robot.py
@@ -8,6 +8,24 @@ class Robot:
         self.y = y
         self.direction = direction
         self.couleur = couleur
-        self.points_degat = 0
-        self.vies = 3
+        self.points_degat = 1
+        self.pv = 3
         self.est_detruit = False
+
+    def deplacer(self, direction: Direction):
+
+        if direction == Direction.NORD:
+            self.y -= 1
+        elif direction == Direction.EST:
+            self.x += 1
+        elif direction == Direction.SUD:
+            self.y += 1
+        elif direction == Direction.OUEST:
+            self.x -= 1
+
+    def recoit_degats(self,degats: int):
+        if self.pv <= degats :
+            self.est_detruit = True
+        else:
+            self.pv -= degats
+
